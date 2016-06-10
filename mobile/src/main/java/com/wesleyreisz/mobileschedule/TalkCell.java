5efb2dc8a67ada5b23e9d6a3b6b68953d54b9cd8
Wesley Reisz <wes@wesleyreisz.com>
2016-06-10 09:02:01 -0400

    Implemented a custom composite layout
    Each of the talk cells implement a customer composite layout.
    This approach uses merge and an otherwise component layout that
    would make the xml very busy. This is a good lecture and exercise
    for students to use. Prior to doing a custom layout.

---
+++ mobile/src/main/java/com/wesleyreisz/mobileschedule/TalkCell.java
@@ -0,0 +1,41 @@
+package com.wesleyreisz.mobileschedule;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by wesleyreisz on 6/10/16.
+ */
+public class TalkCell extends LinearLayout {
+    private final Context context;
+    private final AttributeSet attrs;
+
+    public TalkCell(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            this.context = context;
+            this.attrs = attrs;
+
+            setOrientation(VERTICAL);
+            setGravity(Gravity.CENTER);
+
+            LayoutInflater.from(context).inflate(R.layout.talk_cell, this, true);
+
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TalkCell, 0, 0);
+
+            String textTalkTitle = array.getString(R.styleable.TalkCell_talkTitle);
+            if (textTalkTitle == null) textTalkTitle = "No Title Set";
+            ((TextView) findViewById(R.id.textTalkTitle)).setText(textTalkTitle);
+
+            String textSpeakerName = array.getString(R.styleable.TalkCell_speakerName);
+            if (textSpeakerName == null) textTalkTitle = "No Title Set";
+            ((TextView) findViewById(R.id.textSpeakerName)).setText(textSpeakerName);
+
+            array.recycle();
+        }
+    }
+
